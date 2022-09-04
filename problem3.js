203aac0512d5b0613ed9676b7408bc9ec7826e38
simran028 <simrangupta9082@gmail.com>
2022-09-05 01:55:23 +0530

    Introduction to JS(variables) problems

---
+++ problem3.js
@@ -0,0 +1,15 @@
+
+
+// Create a variable name to store your name
+// Create a variable age to store your age
+// Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name = "Simran Gupta";
+
+let age = 21;
+
+console.log(name , 'is' , age , 'year old' );
+
+console.log(typeof(name));
+
+console.log(typeof(age));
---
+++ problem4.js
@@ -0,0 +1,27 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+
+let name = "Simran Gupta";
+
+let school = "Holy Infant High School";
+
+let grade = "A";
+
+let Rollno = "67";
+
+let subject1 =("Science-60");
+let subject2=("English-50");
+let subject3=("Math-50");
+
+
+console.log("***","𝕣𝕖𝕡𝕠𝕣𝕥 𝕔𝕒𝕣𝕕","***")
+console.log(1,"name-Simran Gupta");
+console.log(2,"school-Holy Infant High School");
+console.log(3,"Section-A")
+console.log(4,"grade-A");
+console.log(5,"Rollno-67");
+console.log(6,subject1);
+console.log(7,subject2);
+console.log(8,subject3);
